bfcebd604c849ee79c16882034d17e6e39850a7b
Harry Paek <paek.harry@eplan.co.kr>
2021-07-07 16:56:15 +0900

    Modified Log4Net support, changed Log4Net init position to Init() to support all EPLAN versions

--- EPLAN API Examples/ApiExtensionExamples/Eplan.EplAddin.ApiSampleAddin/SampleAddInModule.cs
+++ EPLAN API Examples/ApiExtensionExamples/Eplan.EplAddin.ApiSampleAddin/SampleAddInModule.cs
@@ -16,6 +16,7 @@ namespace Eplan.EplAddin.ApiSampleAddin
 
     public class SampleAddInModule : IEplAddIn, IEplAddInShadowCopy
     {
+        private readonly string _log4netConfigFile = ".\\Config\\log4net.config";
         private ILog _logger = null;
         private string _originalAssemblyPath = string.Empty;
         private string _shadowAssemblyPath = string.Empty;
@@ -27,8 +28,6 @@ namespace Eplan.EplAddin.ApiSampleAddin
             try
             {
                 this._shadowAssemblyPath = Path.GetDirectoryName(this.GetType().Assembly.Location);
-
-                InitializeLogging();
 
                 if (this._logger != null)
                     this._logger.InfoFormat("Constructor(), initializing Add-In...[{0}]", this._shadowAssemblyPath);
@@ -48,6 +47,8 @@ namespace Eplan.EplAddin.ApiSampleAddin
         {
             if (this._logger != null)
                 this._logger.InfoFormat("OnInit()@{0}", DateTime.Now);
+
+            InitializeLogging();
 
             this._symbolPartChangedEventListener = new SymbolPartChangedActionEventListener(null);
 
@@ -135,7 +136,8 @@ namespace Eplan.EplAddin.ApiSampleAddin
         /// <returns>초기화에 사용된 Config File Full Path</returns>
         private string InitializeLogging()
         {
-            string configFileFullPath = Path.Combine(this._shadowAssemblyPath, ".\\Config\\log4net.config");
+            string configFileFullPath = Path.Combine(this._shadowAssemblyPath, this._log4netConfigFile);
+            EnsureLog4NetConfigurationFile(configFileFullPath);
 
             LoggerFactory.InitializeLogging(configFileFullPath);
             this._logger = LoggerFactory.GetLogger(this.GetType());
@@ -144,6 +146,30 @@ namespace Eplan.EplAddin.ApiSampleAddin
                 this._logger.DebugFormat("InitializeLogging(), LoggerFactory Initialized!!!, configFileFullPath=[{0}]", configFileFullPath);
 
             return configFileFullPath;
+        }
+
+        /// <summary>
+        /// EPLAN Version에 따라 Log4Net Configuration 파일이 복사되지 않는 경우가 있음
+        /// - 실제 위치에 존재하는지 확인하고 없으면 설치 위치에서 복사해 옴
+        /// </summary>
+        private void EnsureLog4NetConfigurationFile(string configFileFullPath)
+        {
+            if (File.Exists(configFileFullPath))
+                return;
+
+            // if NOT, Copy from original path
+            try
+            {
+                string configFolder = Path.GetDirectoryName(configFileFullPath);
+
+                if (!Directory.Exists(configFolder))
+                    Directory.CreateDirectory(configFolder);
+
+                File.Copy(Path.Combine(this._originalAssemblyPath, this._log4netConfigFile), configFileFullPath, true);
+            }
+            catch
+            {
+            }
         }
 
         #endregion
